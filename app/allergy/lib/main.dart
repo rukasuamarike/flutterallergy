a6122c0daa78b91ad54630c1e02d199e014c5293
RaphaLK <raphaelkusuma5@gmail.com>
2023-02-19 02:36:25 -0800

    added an awaiting camera function

--- app/allergy/lib/main.dart
+++ app/allergy/lib/main.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_camera_practice/home_page_dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const algScan());
+  final cameras = await availableCameras();
+}
+
+await availableCameras().then(
+  (value) => Navigator.push(
+    context, MaterialPageRoute(
+      builder: (_) => CameraPage(cameras: value)
+    ),
+  )
+);
+
+class algScan extends StatefulWidget{
+  const algScan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      title: 'Flutter Camera Demo',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+
+}
 
 class Home extends StatefulWidget {
   const Home({super.key});
