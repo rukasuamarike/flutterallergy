fc1f27da618635ee0576970e6aa52e8260e5807c
Lucas Amlicke <lukeamlicke@gmail.com>
2023-02-19 02:12:55 -0800

    fix

---
+++ app/allergy/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
