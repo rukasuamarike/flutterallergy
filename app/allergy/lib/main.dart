af4c591d02d33c5a39aea12c46c8967e03b15fe7
Lucas Amlicke <lukeamlicke@gmail.com>
2023-02-19 02:33:14 -0800

    main

--- app/allergy/lib/main.dart
+++ app/allergy/lib/main.dart
@@ -1,6 +1,27 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+void main() async {
+
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Home(),)
+  }
+}
 class Home extends StatefulWidget {
   const Home({super.key});
 
