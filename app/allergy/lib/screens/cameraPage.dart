ad375805433d69b71ae014b91e70ef7c48ea9233
Lucas Amlicke <lukeamlicke@gmail.com>
2023-02-19 07:26:13 -0800

    CAMERA WORKS!!

--- app/allergy/lib/screens/cameraPage.dart
+++ app/allergy/lib/screens/cameraPage.dart
@@ -1,9 +1,12 @@
+import 'package:allergy/screens/PreviewPage.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
 
 class CameraApp extends StatefulWidget {
   /// Default Constructor
@@ -21,6 +24,28 @@ class CameraApp extends StatefulWidget {
 
 class _CameraAppState extends State<CameraApp> {
   late CameraController controller;
+
+  Future takePicture() async {
+    if (!controller.value.isInitialized) {
+      return null;
+    }
+    if (controller.value.isTakingPicture) {
+      return null;
+    }
+    try {
+      await controller.setFlashMode(FlashMode.off);
+      XFile picture = await controller.takePicture();
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => PreviewPage(
+                    picture: picture,
+                  )));
+    } on CameraException catch (e) {
+      debugPrint('Error occured while taking picture: $e');
+      return null;
+    }
+  }
 
   @override
   void initState() {
@@ -57,7 +82,25 @@ class _CameraAppState extends State<CameraApp> {
       return Container();
     }
     return MaterialApp(
-      home: CameraPreview(controller),
-    );
+        home: Scaffold(
+            body: SafeArea(
+                child: Stack(children: [
+      CameraPreview(controller),
+      Positioned(
+          left: (MediaQuery.of(context).size.width / 2) - 10,
+          bottom: MediaQuery.of(context).size.height / 5,
+          child: Container(
+              decoration: const BoxDecoration(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+            color: Colors.white,
+          ))),
+      Positioned(
+          left: (MediaQuery.of(context).size.width / 2) - 10,
+          bottom: MediaQuery.of(context).size.height / 5,
+          child: IconButton(
+              iconSize: 20.0,
+              onPressed: () => {takePicture()},
+              icon: Icon(Icons.camera_alt_rounded))),
+    ]))));
   }
 }
